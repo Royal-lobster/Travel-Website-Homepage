b13faba72caa88e06d81c185ea9e23cb8a292baa
Royal-lobster <srujangs8@gmail.com>
2021-06-29 14:25:09 +0530

    fixed fonts loading in layout to document by next lint

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Head from "next/head";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
@@ -10,14 +9,6 @@ import _JSXStyle from "styled-jsx/style";
 function Layout({ children }) {
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,600;0,700;0,800;1,400&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <div className="layout">
         <div>
           <Navbar />
